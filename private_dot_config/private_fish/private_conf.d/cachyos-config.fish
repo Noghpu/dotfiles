3912c9c9bc50a793d98735a13e75b1dd094e5b63
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-02-05 07:59:38 +0100

    update

--- private_dot_config/private_fish/private_conf.d/cachyos-config.fish
+++ private_dot_config/private_fish/private_conf.d/cachyos-config.fish
@@ -62,27 +62,27 @@ alias lt='eza -aT --color=always --group-directories-first --icons=always' # tre
 alias l.="eza -a | grep -e '^\.'" # show only dotfiles
 
 # Common use
-abbr -a grubup "sudo grub-mkconfig -o /boot/grub/grub.cfg"
-abbr -a fixpacman "sudo rm /var/lib/pacman/db.lck"
-abbr -a tarnow 'tar -acf '
-abbr -a untar 'tar -zxvf '
-abbr -a wget 'wget -c '
-abbr -a psmem 'ps auxf | sort -nr -k 4'
-abbr -a psmem10 'ps auxf | sort -nr -k 4 | head -10'
+abbr -a grubup sudo grub-mkconfig -o /boot/grub/grub.cfg
+abbr -a fixpacman sudo rm /var/lib/pacman/db.lck
+abbr -a tarnow tar -acf
+abbr -a untar tar -zxvf
+abbr -a wget wget -c
+abbr -a psmem ps auxf | sort -nr -k 4
+abbr -a psmem10 ps auxf | sort -nr -k 4 | head -10
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
-abbr -a dir 'dir --color=auto'
-abbr -a vdir 'vdir --color=auto'
-abbr -a grep 'grep --color=auto'
-abbr -a fgrep 'fgrep --color=auto'
-abbr -a egrep 'egrep --color=auto'
-abbr -a hw 'hwinfo --short' # Hardware Info
+abbr -a dir dir --color=auto
+abbr -a vdir vdir --color=auto
+abbr -a grep grep --color=auto
+abbr -a fgrep fgrep --color=auto
+abbr -a egrep egrep --color=auto
+abbr -a hw hwinfo --short # Hardware Info
 abbr -a big "expac -H M '%m\t%n' | sort -h | nl" # Sort installed packages according to size in MB
-abbr -a gitpkg 'pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
-abbr -a update 'sudo cachyos-rate-mirrors && sudo pacman -Syu'
+abbr -a gitpkg "pacman -Q | grep -i "\-git" | wc -l" # List amount of -git packages
+abbr -a update "sudo cachyos-rate-mirrors && sudo pacman -Syu"
 
 # Get fastest mirrors
 #alias mirror="sudo cachyos-rate-mirrors"
@@ -98,7 +98,7 @@ if type -q pacman
 end
 
 # Get the error messages from journalctl
-abbr -a jctl="journalctl -p 3 -xb"
+abbr -a jctl "journalctl -p 3 -xb"
 
 # Recent installed packages
-abbr -a rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+abbr -a rip "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
