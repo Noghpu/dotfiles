4150d0411388540e3e1730f29dc9c2a1137760df
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-02-05 10:50:40 +0100

    update

---
+++ private_dot_config/private_nvim/private_lua/private_plugins/blink.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<Down>"] = {},
+        ["<Up>"] = {},
+        ["<Left>"] = {},
+        ["<Right>"] = {},
+      },
+      completion = {
+        ghost_text = { enabled = false },
+        trigger = { prefetch_on_insert = false },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    },
+  },
+}
