a220dc390c7843ad97db1607241c8eb3c2b3193c
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-03-01 23:01:57 +0100

    fix winuv

--- private_dot_config/exact_private_fish/exact_private_functions/private_winuv.fish
+++ private_dot_config/exact_private_fish/exact_private_functions/private_winuv.fish
@@ -1,6 +1,7 @@
 function winuv
-    set wsl_path (pwd)
-    set win_path (string replace --regex '^/home' 'W:\\home' $wsl_path | string replace --all '/' '\\')
-    set uv_exe /mnt/c/Users/$USER/scoop/shims/uv.exe
+    set -l wsl_path (pwd)
+    set -l win_path (string replace '/home' 'G:\\home' $wsl_path | string replace --all '/' '\\')
+    echo $win_path
+    set -l uv_exe /mnt/c/Users/$USER/scoop/shims/uv.exe
     $uv_exe run --directory $win_path $argv
 end
