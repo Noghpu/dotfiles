53cd95334823dbb15eedb6976fb7c17c4431dc3e
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-03-13 17:37:20 +0100

    add abbrs

--- private_dot_config/exact_private_fish/exact_private_conf.d/private_zmx.fish
+++ private_dot_config/exact_private_fish/exact_private_conf.d/private_zmx.fish
@@ -1,4 +1,8 @@
 if type -q zmx
+    abbr --add mx zmx
+    if test -n "$ZMX_SESSION"
+        abbr --add detach zmx detach
+    end
     if not test -f ~/.config/fish/completions/zmx.fish
         echo 'zmx completions fish | source' >~/.config/fish/completions/zmx.fish
     end
