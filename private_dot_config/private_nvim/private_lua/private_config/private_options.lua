06b71b7913141bee755c5e7b613399110412c2cd
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-03-13 14:20:31 +0100

    rewrite register handling to work with delete as well

--- private_dot_config/private_nvim/private_lua/private_config/private_options.lua
+++ private_dot_config/private_nvim/private_lua/private_config/private_options.lua
@@ -1,36 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
--- vim.g.lazyvim_python_lsp = "basedpyright"  -- replaced by ty in lua/plugins/python-lsp.lua
 vim.g.snacks_animate = false
+
+-- Prefer fish > nu > system default
 if vim.fn.executable("fish") == 1 then
-	vim.o.shell = "fish"
+  vim.o.shell = "fish"
 elseif vim.fn.executable("nu") == 1 then
-	vim.o.shell = "nu"
+  vim.o.shell = "nu"
 end
 
 -- OSC 52 clipboard for SSH yanking (async, no paste delay)
 local is_windows = vim.env.LC_CLIENT_OS == "windows"
 if is_windows then
-	vim.g.clipboard = {
-		name = "OSC 52",
-		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-		},
-		paste = {
-			["+"] = function()
-				---@diagnostic disable-next-line: redundant-parameter
-				return { vim.fn.getreg("0", 1, true), vim.fn.getregtype("0") }
-			end,
-			["*"] = function()
-				-- Fall back to "0 register for fast paste
-				---@diagnostic disable-next-line: redundant-parameter
-				return { vim.fn.getreg("0", 1, true), vim.fn.getregtype("0") }
-			end,
-		},
-	}
+  local osc52 = require("vim.ui.clipboard.osc52")
 
-	-- Set after g:clipboard so Neovim uses the custom provider
-	vim.o.clipboard = "unnamedplus"
+  local function paste()
+    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+
+  vim.o.clipboard = "unnamedplus"
 end
