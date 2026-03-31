ccf115a4e75e3a89c7c4bab0792e8087fb3f1986
Phuong Dao-Donnelly <phuong.dao@freiberger.com>
2026-03-31 13:01:07 +0200

    fix: copy templates contents

--- private_dot_config/exact_private_fish/exact_private_functions/private_apply_typing_templates.fish
+++ private_dot_config/exact_private_fish/exact_private_functions/private_apply_typing_templates.fish
@@ -1,5 +1,5 @@
 function apply_typing_templates
     git clone --depth 1 https://github.com/Noghpu/python-typing-templates /tmp/typing-tpl \
-        && cp -rf /tmp/typing-tpl/ . \
+        && cp -rf /tmp/typing-tpl/* . \
         && rm -rf /tmp/typing-tpl
 end
