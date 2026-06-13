18ff5ec90de160dfecaf50e3b75c564cc40bcb15
Dao, Phuong <Phuong.Dao@freiberger.com>
2026-06-13 11:31:11 +0200

    fix fish overspawning agents

--- private_dot_config/exact_private_fish/exact_private_conf.d/private_ssh.fish
+++ private_dot_config/exact_private_fish/exact_private_conf.d/private_ssh.fish
@@ -6,7 +6,13 @@ if test -n "$SSH_AUTH_SOCK"; and ssh-add -l >/dev/null 2>&1
 end
 
 # Start a fresh agent
-eval (ssh-agent -c) >/dev/null
+if status is-interactive
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.sock
+    if not test -S $SSH_AUTH_SOCK
+        ssh-agent -c -a $SSH_AUTH_SOCK >/dev/null
+    end
+    # ssh-add ~/.ssh/id_ed25519 2>/dev/null
+end
 
 # Add default key if it exists
 if test -f ~/.ssh/id_ed25519
